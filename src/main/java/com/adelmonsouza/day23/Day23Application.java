cfb16365f9606adeaab3774d73e796b4e9199984
Adelmon Souza <adelmon.souza@outlook.com>
2025-12-01 17:55:34 +0000

    feat: Adicionar estrutura Spring Boot Day 23 - GraphQL API com Spring Boot

---
+++ src/main/java/com/adelmonsouza/day23/Day23Application.java
@@ -0,0 +1,17 @@
+package com.adelmonsouza.day23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Day 23 of #30DiasJava
+ * GraphQL API com Spring Boot
+ * 
+ * queries, mutations e subscriptions
+ */
+@SpringBootApplication
+public class Day23Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Day23Application.class, args);
+    }
+}
